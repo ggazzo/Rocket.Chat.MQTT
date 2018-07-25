5c6d015a3caa7e905fc5688c908d62c0c8cdfce5
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-07-25 09:28:20 -0300

    redis config

--- src/server.js
+++ src/server.js
@@ -5,18 +5,23 @@ import { init, connect } from './index';
 const url = process.env.MONGO_URL || 'mongodb://localhost:27017';
 const dbName = process.env.MONGO_DB || 'meteor';
 
+const port = process.env.REDIS_PORT || 6379;
+const host = process.env.REDIS_HOST || 'localhost';
+
+const config = { port, host };
+
 MongoClient.connect(url,async function(err, client) {
 	if (err) process.exit(1);
 
 	const db = client.db(dbName);
 
 	const options = init({
-		Subscriptions: db.collection("rocketchat_subscription")
+		Subscriptions: db.collection("rock;etchat_subscription")
 	});
 
 	connect({
 		...options,
-		mq: require("mqemitter-redis")(),
-		persistence: require("aedes-persistence-redis")()
+		mq: require("mqemitter-redis")(config),
+		persistence: require("aedes-persistence-redis")(config)
 	});
 });
