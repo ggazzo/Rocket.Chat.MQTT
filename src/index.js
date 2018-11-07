be62ea3512f221e473a091ed3dcecf087d1e883c
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-11-07 19:54:42 -0200

    added logic anonymous and livechat

--- src/index.js
+++ src/index.js
@@ -10,56 +10,71 @@ const host = process.env.REDIS_HOST || 'localhost';
 
 const config = { port, host };
 
-export const createService = () => {
+export const createService = ({ mq }) => new ServiceBroker({
+	logLevel: 'info',
+	sampleCount: 1,
+	namespace: 'services',
+	metrics,
+	transporter: 'TCP',
+	cacher: 'Memory',
+	started(Services) {
+		MqttServer.connect({
+			async authorizeSubscribe(client, sub, callback) {
+				try {
+					// const { topic } = sub;
+					if (!(await Routes.Subscriptions.validate(client, sub, Services))) {
+						throw 'not authorized';
+					}
+					return callback(null, sub);
+				} catch (error) {
+					Services.logger.log(error);
+					callback(error);
+				}
+			},
+			async authorizePublish(/* client, packet, callback*/) {
+				return false;
+			},
+			async authenticate(client, username, password, callback) {
+				try {
+					// AnonymousRead
+					if (!password && username === 'anonymous') {
+						if (await Services.call('settings.get', { name: 'Accounts_AllowAnonymousRead' })) {
+							client.user = { };
+							callback(null, {});
+						}
 
-	return new ServiceBroker({
-		logLevel: 'info',
-		sampleCount: 1,
-		namespace: 'services',
-		metrics,
-		transporter: 'TCP',
-		cacher: 'Memory',
-		started(Services) {
-			MqttServer.connect({
-				async authorizeSubscribe(client, sub, callback) {
-					try {
-						// const { topic } = sub;
-						if (!(await Routes.Subscriptions.validate(client, sub, Services))) {
-							throw 'not authorized';
-						}
-						return callback(null, sub);
-					} catch (error) {
-						console.log(error);
-						callback(error);
+						return callback(true);
 					}
-				},
-				async authorizePublish(/* client, packet, callback*/) {
-					return false;
-				},
-				async authenticate(client, username, password, callback) {
-					try {
-						const user = await Services.call('authentication.login', { username, password: password.toString('utf8') });
-						if (user) {
+
+					// LiveChat
+					if (username === 'livechat-guest') {
+						if (await Services.call('settings.get', { name: 'Livechat_enabled' })) {
+							const user = await Services.call('authentication.loginJWT', { token : password });
 							client.user = user;
-							client.user._id = 'rocket.cat';
-							return callback(false, !!user);
+							callback(null, {});
 						}
-						// jwt.verify(username, JWT_SECRET, function (err, decoded) {
-						// 	// console.log('decoded ->', decoded);
-						// 	callback(err, !!client.user);
-						// });
-						callback(true);
-					} catch (error) {
-						console.log(error);
-						callback(error);
+
+						return callback(true);
 					}
-				},
-				mq: require('mqemitter-redis')(config),
-				persistence: require('aedes-persistence-redis')(config),
-			});
-		}
-	});
-}
+
+					// Users
+					const user = await Services.call('authentication.login', { username, password: password.toString('utf8') });
+					if (user) {
+						client.user = user;
+						return callback(false, !!user);
+					}
+
+					callback(true);
+				} catch (error) {
+					Services.logger.log(error);
+					callback(error);
+				}
+			},
+			mq: mq || require('mqemitter-redis')(config),
+			persistence: require('aedes-persistence-redis')(config),
+		});
+	},
+});
 if (require.main === module) { // standalone
 	createService().start();
 }
